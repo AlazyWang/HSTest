49762d9f9b27ff16a3d02c4ea327850bd64c2d93
AlazyWang <AlazyWang@AlazyWang.local>
2018-12-03 11:40:36 +0800

    xxx

--- HSTest/AppDelegate.m
+++ HSTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Dog.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    Dog *dog = [[Dog alloc]init];
+    
     return YES;
 }
 
