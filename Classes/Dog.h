6b111bc500a827bdaa8a1e7d669cec59f36e8696
AlazyWang <AlazyWang@AlazyWang.local>
2018-12-03 10:44:50 +0800

    xxx

---
+++ Classes/Dog.h
@@ -0,0 +1,17 @@
+//
+//  Dog.h
+//  HSTest
+//
+//  Created by AlazyWang on 2018/12/3.
+//  Copyright © 2018年 AlazyWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  HSTest
+//
+//  Created by AlazyWang on 2018/12/3.
+//  Copyright © 2018年 AlazyWang. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
